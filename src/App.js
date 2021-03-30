79d25a1f8829a330a8d258f34286f820a86e473b
portduport <pablo@callejo.fr>
2021-03-30 15:09:22 +0200

    add image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { app } from "./firebase";
 import { Button } from "@material-ui/core";
 import firebase from "firebase";
 
 function App() {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    app.auth().onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, []);
   const provider = new firebase.auth.GoogleAuthProvider();
   const authWithGoogle = () => {
     firebase.auth().signInWithPopup(provider);
@@ -13,18 +19,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {user && (
+          <>
+            <img
+              src={user.photoURL}
+              width="100px"
+              height="100px"
+              alt="Avatar"
+            />
+            <p>{user.displayName}</p>
+          </>
+        )}
         <Button variant="contained" color="primary" onClick={authWithGoogle}>
           LOGIN
         </Button>
--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,11 @@ import firebase from "firebase";
 import "firebase/auth";
 import "firebase/firestore";
 
-const firebaseConfig = {
+export const app = firebase.initializeApp({
   apiKey: "AIzaSyB5B7ow-4eqrf5_pmB0hiq34i2j3MHyMl4",
   authDomain: "rent-alpes-6b66d.firebaseapp.com",
   projectId: "rent-alpes-6b66d",
   storageBucket: "rent-alpes-6b66d.appspot.com",
   messagingSenderId: "851241372390",
   appId: "1:851241372390:web:4ba71e6e3ef5fedec60927",
-};
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-export default app;
+});
