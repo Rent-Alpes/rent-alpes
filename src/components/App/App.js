3373c560d11d738ddde7e236827473ec012f138e
portduport <pablo@callejo.fr>
2021-05-03 10:39:36 +0200

    write property in firestore

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,28 @@
 import "../../App.css";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "../Signup/Signup";
 import Login from "../Login/Login";
 import ForgetPassword from "../ForgetPassword/ForgetPassword";
+import Property from "../Property/PropertyForm";
+import app from "firebase/app";
 
 function App() {
-
+  const addOrEdit = async (propertyObject) => {
+    console.log(propertyObject);
+    await app.firestore().collection("Property").doc().set(propertyObject);
+  };
   return (
     <Router>
-
       <Switch>
         <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
         <Route path="/forgetpassword" component={ForgetPassword} />
+        <Route path="/property">
+          <Property addOrEditProperty={addOrEdit} />
+        </Route>
 
         {/* PENSER A CREER UN COMPOSANT POUR MAUVAIS PATH */}
       </Switch>
-
     </Router>
   );
 }
