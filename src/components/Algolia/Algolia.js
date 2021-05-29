60a7fe59ece5481d75f62d838975b483882f6fb3
David <dserra@aptesia.fr>
2021-05-29 16:12:24 +0200

    Algolia search property

--- src/components/Algolia/Algolia.js
+++ src/components/Algolia/Algolia.js
@@ -4,26 +4,42 @@ import app from "firebase/app";
 
   const client = algoliasearch('WM2R73MT8D', '8f1fea58043949f2ba7714b32998a65d');
   const index = client.initIndex("Property");
+  var resultProperty;
+
+
+export const AddProperty = (propertyValues, propertyId) => {
+  const objects = propertyValues;
+  objects.objectID = propertyId;
+  index.saveObjects(objects);
+};
+
+export const SearchProperty = (searchText) => {
+
+ SearchSame(searchText);
+ console.log(resultProperty);
 
  
+    // index.search(searchText, {
+    //   minWordSizefor1Typo: 3,
+    //   minWordSizefor2Typos: 3,
+    //   allowTyposOnNumericTokens: true
+    // })
+    //   .then(({ hits }) => {
+    //     console.log("No result");
+    //     console.log(hits);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
+  
 
-  export const AddProperty = (propertyValues, propertyId) => {
-    const objects  = [
-      {
-      objectID :propertyId,
-      name: propertyValues.name,
-      address: propertyValues.address,
-      postalCode: propertyValues.postalCode,
-      city: propertyValues.city,
-      country: propertyValues.country,
-      bathroom: propertyValues.bathroom,
-      description: propertyValues.description,
-      equipments: propertyValues.equipments,
-      room: propertyValues.room,
-      traveler: propertyValues.traveler,
-    }
-  ];
-    index.saveObjects(objects);
-  };
+};
 
-
+function SearchSame(searchText) {
+  index.search(searchText)
+    .then(({ hits }) => {
+      resultProperty = hits;
+    }).catch(err => {
+      console.log(err);
+    });
+}
