a176610862b2cebb83c2dd73874a21f61f79187a
Clément BURLAT <burlatc73@gmail.com>
2021-06-11 10:04:53 +0200

    profil avec un E

--- src/components/Profil/Profil.js
+++ src/components/Profil/Profil.js
@@ -106,7 +106,7 @@ const Profil = (props) => {
       <div className="bg-gray-50 min-h-screen flex flex-col">
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
           <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-            <h1 className="mb-8 text-3xl text-center">Profil</h1>
+            <h1 className="mb-8 text-3xl text-center">Profile</h1>
             <form onSubmit={handleSubmit}>
               <input
                 type="text"
