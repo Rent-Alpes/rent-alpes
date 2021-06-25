97fdb8e2ee392c37b9513d837a241fae2874f57b
David <David@DAV>
2021-06-25 18:56:27 +0200

    start buger menu

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -67,6 +67,10 @@ const searchClick = ()=>{
     }
   };
 
+  function OpenMenuFilter(event) {
+    document.getElementById("burger-menu").classList.toggle("change");
+    
+  }
 
   const { query } = resarchdata.query;
   return (
@@ -80,20 +84,13 @@ const searchClick = ()=>{
         onChange={handleChange}
         className="static w-full pl-7 rounded-md h-14 text-xl border-2 border-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-600 bg-white bg-opacity-90"
       />
-      {/* <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-    <div class="relative py-3 sm:max-w-xl mx-auto">
-        <nav x-data="{ open: false }">
-            <button class="text-gray-500 w-10 h-10 relative focus:outline-none bg-white" @click="open = !open">
-                <span class="sr-only">Open main menu</span>
-                <div class="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
-                    <span aria-hidden="true" class="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out" :class="{'rotate-45': open,' -translate-y-1.5': !open }"></span>
-                    <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-current   transform transition duration-500 ease-in-out" :class="{'opacity-0': open } "></span>
-                    <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out" :class="{'-rotate-45': open, ' translate-y-1.5': !open}"></span>
-                </div>
-            </button>
-        </nav>
-    </div>
-</div> */}
+
+<div className="container" id="burger-menu" onClick={OpenMenuFilter}>
+  <div className="bar1"></div>
+  <div className="bar2"></div>
+  <div className="bar3"></div>
+</div>
+
       <Checkbox id="filter-check" onChange={handleChange}></Checkbox>
       <br />
       <br />
