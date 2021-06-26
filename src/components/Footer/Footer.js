8ecb6f6eb4205eda63e1b025ef0ad353fef401c3
portduport <pablo@callejo.fr>
2021-06-26 21:59:17 +0200

    footer css corrections

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,24 @@
 import { BiCheckShield } from "react-icons/bi";
 
 const Footer = () => (
-  <div className="footer flex justify-end">
-    <div className="mr-10">
+  <div className="footer flex justify-center items-center">
+    <div className="ml-32 flex-grow">
       <p>
         <strong>Rent'Alpes © All rights reserved 2021</strong>
       </p>
     </div>
-    <div className="mr-5">
-      <p>
+    <div
+      className="mr-5 items-center  flex justify-center "
+      style={{ backgroundColor: "#ff9f00", width: "6em", height: "3.8em" }}
+    >
+      <div>
         <a href="/privacy">
           <div className="flex items-center">
             <BiCheckShield />
             <strong className="ml-1">Privacy</strong>
           </div>
         </a>
-      </p>
+      </div>
     </div>
   </div>
 );
